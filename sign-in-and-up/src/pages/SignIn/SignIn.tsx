7df6021065e0ce52e2df7ce554704c0b6da307c7
Taehee_Hwang <tevem1207@gmail.com>
2023-05-03 15:44:41 +0900

    Refactor signin

--- sign-in-and-up/src/pages/SignIn/SignIn.tsx
+++ sign-in-and-up/src/pages/SignIn/SignIn.tsx
@@ -1,11 +1,4 @@
-import { Link as RouterLink } from 'react-router-dom';
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
@@ -13,14 +6,6 @@ import Container from '@mui/material/Container';
 import SignInForm from 'components/auth/SignInForm';
 
 export default function SignIn() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -36,7 +21,7 @@ export default function SignIn() {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Sign In
+          로그인
         </Typography>
         <SignInForm />
       </Box>
